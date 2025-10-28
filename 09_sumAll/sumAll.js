5c4e7aec3863e372e9440dd92e16c77c8a81776a
END1502 <86548217+END1502@users.noreply.github.com>
2025-10-28 21:02:59 +0800

    Solved: act09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    // get all number to add
+    let sum = 0;
 
+    if ((start < 0 || end < 0) ||
+        (!Number.isSafeInteger(start) ||
+            !Number.isSafeInteger(end))) {
+        return "ERROR"
+    }
+
+    if (start < end) {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+    } else if (start > end) {
+        for (let i = start; i >= end; i--) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
