6c0b34feff42568e0ddc830c7254cfda870e22ec
END1502 <86548217+END1502@users.noreply.github.com>
2025-10-28 20:27:12 +0800

    Solved: act08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr) {
+    let toRemoveItem = [];
+    let removedItem = [];
+
+    for (let i = 1; i < arguments.length; i++) {
+        toRemoveItem.push(arguments[i]);
+    }
+
+    for (item of toRemoveItem) {
+        const toRemoveIndex = arr.findIndex((num) => num === item);
+        arr = arr.filter((num) => num !== item);
+        removedItem = arr;
+    }
+
+    return removedItem;
 };
 
 // Do not edit below this line
