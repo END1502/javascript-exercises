46e6e3b94dce6c570ca3c892c2b6ffc61651d6e9
END1502 <86548217+END1502@users.noreply.github.com>
2025-10-28 15:39:07 +0800

    Solved: act07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (word) {
+    const wordArr = [...word];
+    let reversed = [];
 
+    for (let i = wordArr.length; i >= 0; i--){
+        reversed += [wordArr[i]];
+    }
+    
+    return reversed;
 };
 
 // Do not edit below this line
