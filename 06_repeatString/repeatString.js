d50ef11f20c5c9b559f39ac4af79c69f73f5dab9
END1502 <86548217+END1502@users.noreply.github.com>
2025-10-28 15:14:04 +0800

    Solved: act06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+    let msg = "";
+    if (times >= 0) {
+        for (let i = 0; i < times; i++) {
+            msg += word;
+        }
+    } else {
+        msg = "ERROR";
+    }
+    return msg;
 };
 
 // Do not edit below this line
